1e536ebb367f136b4ff5c26335d51b50c658a0e5
hongcheng4ni <46550415+hongcheng4ni@users.noreply.github.com>
2019-01-10 12:55:20 +0800

    Create answer_3.c

---
+++ answer_3.c
@@ -0,0 +1,20 @@
+int* nextFibonacci(int* arr){
+  int i;
+  int p_fn, fn, n_fn;
+  int ans[strlen(arr)];
+  
+  for(i=0;i<strlen(arr)){
+    p_fn  = 1;
+    fn  = 1;
+    n_fn  = 2;
+    while(arr[i] != fn){
+      n_fn  = p_fn  + fn;
+      p_fn  = fn;
+      fn  = n_fn;
+    }
+    
+    ans[i]=n_fn;
+  }
+  
+  return ans;
+}
