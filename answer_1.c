068ac62905a117f1a01f93649c8df3494f3d4234
hongcheng4ni <46550415+hongcheng4ni@users.noreply.github.com>
2019-01-10 12:39:44 +0800

    Update answer_1.c

--- answer_1.c
+++ answer_1.c
@@ -1,1 +1,21 @@
-
+bool isSubset(char *arr_1, char *arr_2){
+  int i,j;
+  int *rec = calloc(26,sizeof(int));
+  
+  for(i=0;i<strlen(arr_1);i++){
+    j = (int) arr_1[i] - 65;
+    if(rec[j]==0){
+      rec[j] = 1;
+  }
+  
+  for(i=0;i<strlen(arr_2);i++){
+    j = (int) arr_2 - 65;
+    if(rec[j]==0){
+      return false;
+    }
+  }
+  
+  free(rec);
+  
+  return true;
+}
